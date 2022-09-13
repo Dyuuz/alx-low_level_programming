3091e6313a4798a1a4cefeab1fc5a88df1814d3f
Dyuuz <lekanoyesunle@gmail.com>
2022-09-13 15:44:23 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,39 +7,41 @@
 void jack_bauer(void)
 {
 	int x;
-        int a, b, c, d;
+	int a, b, c, d;
 
-        a = 0; b = 0; c = 0; d = 0;
-        
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
 	x = 0;
-        
-        while (x < 1440)
-        {
-                _putchar(a + '0');
-                _putchar(b + '0');
-                _putchar(':');
-                _putchar(c + '0');
-                _putchar(d + '0');
-                _putchar('\n');
-                
-                d++;
-                
-                if (d > 9)
-                {
-                        d = 0;
-                        c++;
-                }
-                if (c > 5)
-                {
-                        c = 0;
-                        b++;
-                }
-                if (b > 9)
-                {
-                        b = 0;
-                        a++;
-                }
-                x++;
-        }
+
+	while (x < 1440)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+
+		if (d > 9)
+		{
+			d = 0;
+			c++;
+		}
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		x++;
+	}
 }
 
