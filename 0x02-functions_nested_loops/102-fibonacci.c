8225a48f4779b596287a4cf982a29f712207eff3
Dyuuz <lekanoyesunle@gmail.com>
2022-09-13 16:34:56 -0700

    tasks

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,32 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * jack_bauer - Prints the minutes of a day
- *
- * Return: no return
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-void jack_bauer(void)
+int main(void)
 {
-	int a, b, c, d;
+	long int i, j, k, next;
 
-	for (a = 48; a <= 50; a++)
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
 	{
-		for (b = 48; b <= 57; b++)
+		if (j != 20365011074)
 		{
-			for (c = 48; c <= 53; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (a >= 50 && b >= 52)
-						break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
 		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
+
+	return (0);
 }
