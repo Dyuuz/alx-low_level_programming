bde4286628c8424add9283383bb448b4a565e09a
Dyuuz <lekanoyesunle@gmail.com>
2022-10-13 12:21:13 -0700

    sumall

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,27 @@
  *
  * Return: If n == 0, return 0, otherwise sum all the parameters
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int i, sum = 0;
+	unsigned int i = 0;
+	int sum = 0;
+	va_list args;
 
-	va_start(nums, n);
+	va_start(args, n);
+	if (n == 0)
+	{
+		return (0);
+	}
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(nums, int);
-
-	va_end(nums);
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			unsigned int x = va_arg(args, int);
+			sum = sum + x;
+		}
+	}
+	va_end(args);
 
 	return (sum);
 }
